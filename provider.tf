b2115c220ff4548ac35d1afc42c5e7c9f24c7fe5
Kuldeep-805 <kuldeepsingh0650@gmail.com>
2025-08-27 22:20:28 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -10,5 +10,7 @@ terraform {
 provider "azurerm" {
   features {}
   subscription_id = "59bea627-7b7a-4e47-bec4-fb0b51214f26"
-
+  client_id = "88a5638a-4425-414a-81cd-218eeb730d1f"
+  client_secret = "qKu8Q~9ZP0JnYbsW.bALA73yaj~BXQY5AaeOKcuU"
+  tenant_id = "b045856a-3dc6-4e42-9811-6dd226cc9800"
 }
