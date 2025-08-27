1c4ce684f4a574ab28d4924a1d08396d29b117b6
Kuldeep-805 <kuldeepsingh0650@gmail.com>
2025-08-27 21:53:21 +0530

    Update dhondhu.tf

--- dhondhu.tf
+++ dhondhu.tf
@@ -10,10 +10,14 @@ terraform {
 provider "azurerm" {
   features {}
   subscription_id = "59bea627-7b7a-4e47-bec4-fb0b51214f26"
+  client_id = "88a5638a-4425-414a-81cd-218eeb730d1f"
+  client_secret = "qKu8Q~9ZP0JnYbsW.bALA73yaj~BXQY5AaeOKcuU"
+  tenant_id = "b045856a-3dc6-4e42-9811-6dd226cc9800"
 }
 
 resource "azurerm_resource_group" "rg-dhundhu" {
   name     = "rg-dhundhu"
   location = "West Europe"
 }
+
 
